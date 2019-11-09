0e55a4f0987beb9529457671ab636ee76601feaa
longchickenlegs <nabilmansour1999@gmail.com>
2019-11-09 18:27:59 -0500

    Added more notes and left, right movement in Core.

--- Core.py
+++ Core.py
@@ -1,3 +1,23 @@
 import Screen
 # Setting x and y:
-x = 30
+x = 15
+y = 50
+place = 10
+Board = Screen.ScreenMaker(x, y)
+BoardValues = Screen.ScreenValuer(Board)
+Checker = list(BoardValues)
+BoardValues[place] = 'O'
+def Movement(StartingPostion): # wasd
+    postion = StartingPostion
+    while True:
+        Choice = input()
+        if Choice == 'd':
+            BoardValues[postion] = '.'
+            BoardValues[postion + 1] = 'O'
+            postion = postion + 1
+        if Choice == 'a':
+            BoardValues[postion] = '.'
+            BoardValues[postion - 1] = 'O'
+            postion = postion - 1
+        Screen.ScreenRefresher(BoardValues,y,Checker)
+Movement(place)
