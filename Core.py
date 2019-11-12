69452632ed60e3ac29fbcb2560585be46da9f119
longchickenlegs <nabilmansour1999@gmail.com>
2019-11-11 19:49:57 -0500

    Applied raw input ability and employed pixet feat.

--- Core.py
+++ Core.py
@@ -1,34 +1,37 @@
 import Screen
+import msvcrt
+
 # Setting x and y:
 x = 15
 y = 50
 place = 10  # Making an initial position
 # Calling Screen.py
-Board = Screen.ScreenMaker(x, y)
+Board = Screen.ScreenMaker(x, y, '-')
 BoardValues = Screen.ScreenValuer(Board)
 Checker = list(BoardValues)
 BoardValues[place] = 'O'
+Screen.ScreenRefresher(BoardValues, y, Checker, 0)
 
 
 def Movement(StartingPostion, y_Axis_Length):  # wasd for movement
     postion = StartingPostion
     while True:
-        Choice = input()
-        if Choice == 'd':
-            BoardValues[postion] = '.'
-            BoardValues[postion + 1] = 'O'
+        Choice = msvcrt.getch()
+        if str(Choice) == "b'd'":
+            BoardValues[postion] = ' '
+            BoardValues[postion + 1] = '<'
             postion = postion + 1
-        if Choice == 'a':
-            BoardValues[postion] = '.'
-            BoardValues[postion - 1] = 'O'
+        if str(Choice) == "b'a'":
+            BoardValues[postion] = ' '
+            BoardValues[postion - 1] = '>'
             postion = postion - 1
-        if Choice == 's':
-            BoardValues[postion] = '.'
-            BoardValues[postion + y_Axis_Length] = 'O'
+        if str(Choice) == "b's'":
+            BoardValues[postion] = ' '
+            BoardValues[postion + y_Axis_Length] = 'ʌ'
             postion = postion + y_Axis_Length
-        if Choice == 'w':
-            BoardValues[postion] = '.'
-            BoardValues[postion - y_Axis_Length] = 'O'
+        if str(Choice) == "b'w'":
+            BoardValues[postion] = ' '
+            BoardValues[postion - y_Axis_Length] = 'v'
             postion = postion - y_Axis_Length
         Screen.ScreenRefresher(BoardValues, y, Checker, 0)
 
