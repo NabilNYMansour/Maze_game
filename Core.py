dfefb507af4ffafadd675918f2de1fcf0829de49
longchickenlegs <nabilmansour1999@gmail.com>
2019-12-19 23:32:54 -0500

    Tried to make a game over screen.

--- Core.py
+++ Core.py
@@ -10,7 +10,7 @@ place = 10  # Making an initial position
 
 
 #------------------|Calling Screen.py|------------------#
-Board = Screen.ScreenMaker(x, y, '-')
+Board = Screen.ScreenMaker(x, y, '.')
 BoardValues = Screen.ScreenValuer(Board)
 Checker = list(BoardValues)
 BoardValues[place] = 'O'
@@ -18,33 +18,45 @@ Screen.ScreenRefresher(BoardValues, y, Checker, 0)
 #-------------------------------------------------------#
 
 
+#-----------------------|Game Over|----------------------------------------------#
+def GameOver(ScreenValues, Screen_y):
+    '''
+    prints a game over screen given the ScreenValues{list} and Screen_y{int}.
+    '''
+    
+#--------------------------------------------------------------------------------#
+
+
 #----------------------|Movement|--------------------------------------------------------------------------------------------------------------#
-def Movement(ScreenValues, CurrentPosition, Screen_y, trail):  # wasd for movement
+def Movement(ScreenValues, CurrentPosition, Screen_y, trail_horizontal, trail_vertical):  # wasd for movement
     '''
     Allows for the movement of a point on a screen given ScreenValues{list}, the CurrentPosition{int} of the point, and Screen_y{int} values.
     It will also change the point to vʌ<> given the direction input 'wsad' to show a head moving.
-    Can leave a trail{single char. str} behind the point that is moving.
+    Can leave a trail{single char. str} or trail_vertical{single char. str} behind the point that is moving.
+    Will break and output a game over screen if the point hits '_' or '|'.
     '''
     while True:
         Choice = msvcrt.getch()
         if str(Choice) == "b'd'":
-            ScreenValues[CurrentPosition] = trail
+            ScreenValues[CurrentPosition] = trail_horizontal
             ScreenValues[CurrentPosition + 1] = '<'
             CurrentPosition = CurrentPosition + 1
         if str(Choice) == "b'a'":
-            ScreenValues[CurrentPosition] = trail
+            ScreenValues[CurrentPosition] = trail_horizontal
             ScreenValues[CurrentPosition - 1] = '>'
             CurrentPosition = CurrentPosition - 1
         if str(Choice) == "b's'":
-            ScreenValues[CurrentPosition] = trail
+            ScreenValues[CurrentPosition] = trail_vertical
             ScreenValues[CurrentPosition + Screen_y] = 'ʌ'
             CurrentPosition = CurrentPosition + Screen_y
         if str(Choice) == "b'w'":
-            ScreenValues[CurrentPosition] = trail
+            ScreenValues[CurrentPosition] = trail_vertical
             ScreenValues[CurrentPosition - Screen_y] = 'v'
             CurrentPosition = CurrentPosition - Screen_y
         Screen.ScreenRefresher(BoardValues, y, Checker, 0)
 #---------------------------------------------------------------------------------------------------------------------------------------------#
 
 
-Movement(BoardValues, place, y, '*')
+
+
+Movement(BoardValues, place, y, '_', '|')
